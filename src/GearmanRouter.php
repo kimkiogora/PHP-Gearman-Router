16d086ea068e3295d9845faec74a1921c9e4c1dd
Kim Kiogora <kimkiogora@gmail.com>
2015-04-27 09:54:13 +0300

    Create GearmanRouter.php

---
+++ src/GearmanRouter.php
@@ -0,0 +1,26 @@
+
+/**
+ * This file is a router to the gearman service.
+ * Used for synchronous processing of requests.
+ *
+ * PHP VERSION 5.3.6
+ *
+ * @category  Utils
+ * @package   Services Layer
+ * @author    Kim Kiogora   <kimkiogora@gmail.com>
+ * @copyright 2015 Kim Kiogora
+ * @license   Proprietory License
+ * @link      https://github.com/kimkiogora
+ */
+class GearmanRouter {
+    /**
+     * Receives the json encoded data and routes to specific service/function.
+     * 
+     * @param type $json_request
+     */
+    public static function route($log,$requestLogID,$json_request) {
+        $gearman_client = new GearmanClient();
+        $gearman_client->addServer();
+        $result = $gearman_client->doBackground("request",$json_request);
+    }
+}
