9808e26f78f216049404859f39497e06d36b3a7e
Kim Kiogora <kimkiogora@gmail.com>
2015-04-27 09:55:56 +0300

    Update RoutingExample.php

--- src/RoutingExample.php
+++ src/RoutingExample.php
@@ -1,10 +1,22 @@
 <?php
+
+/**
+ * Example
+ * PHP VERSION 5.3.6
+ *
+ * @category  Example
+ * @package   Example
+ * @author    Kim Kiogora   <kimkiogora@gmail.com>
+ * @copyright 2015 Kim Kiogora
+ * @license   Proprietory License
+ * @link      https://github.com/kimkiogora
+ */
 require_once 'GearmanRouter.php';
 /**
-  Add the below line on your transaction processing layer
+  * Add the below line on your transaction processing layer
 */
 $payload = array(
-  "service"=>$serviceName,
+  "service"=>"ExampleA",
   "data"=>array(),//YOUR DATA GOES HERE
 );
 
