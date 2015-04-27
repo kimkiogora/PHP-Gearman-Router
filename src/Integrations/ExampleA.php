d2dc00e88a264d7991d70bc44160588bc0c93243
Kim Kiogora <kimkiogora@gmail.com>
2015-04-27 09:46:46 +0300

    Create ExampleA.php

---
+++ src/Integrations/ExampleA.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is a worker that processes Gearman Requests from any Services Layer.
+ * Used for synchronous processing of requests.
+ *
+ * PHP VERSION 5.3.6
+ *
+ * @category  Utils
+ * @package   Service Layer
+ * @author    Kim Kiogora   <kimkiogora@gmail.com>
+ * @copyright 2015 Kim Kiogora
+ * @license   Proprietory License
+ * @link      https://github.com/kimkiogora
+ */
+class ExampleA {
+
+    /**
+     * Empty
+     */
+    function __construct() {
+    }
+
+    /**
+     * Process transactions.
+     * @param type $data
+     */
+    public function process($data) {
+        $content = json_decode($data[0], true);
+        
+
+        /**
+         * Add processing functionality here, such as send to external
+         * third-party, then close this transaction by invoking postPaymentACK
+         */
+        
+        /**
+         * Close the transaction
+         */
+        
+    }
+}
